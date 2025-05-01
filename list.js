1d411314fe19b1cece216f512d150918d8c16d30
Fleen5l77 <fleen4321@outlook.com>
2025-05-01 10:50:56 +0300

    Add files via upload

---
+++ list.js
@@ -0,0 +1,18 @@
+[
+    {
+        "name": "TextEditor",
+        "category": "Productivity",
+        "description": "Простой текстовый редактор для создания и редактирования текстовых файлов.",
+        "icon": "texteditor/icon.png",
+        "screenshots": ["texteditor/s1.png", "texteditor/s2.png"],
+        "oreUrl": "texteditor/TextEditor.ore"
+    },
+    {
+        "name": "ImageViewer",
+        "category": "Media",
+        "description": "Приложение для просмотра изображений с поддержкой различных форматов.",
+        "icon": "imageviewer/icon.png",
+        "screenshots": ["imageviewer/s1.png"],
+        "oreUrl": "imageviewer/ImageViewer.ore"
+    }
+]
