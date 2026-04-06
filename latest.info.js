ebb9c5512e0e67585698e721fc03b2651282d0b6
Fleen5l77 <fleen4321@outlook.com>
2026-04-06 19:28:31 +0300

    Add files via upload

---
+++ latest.info.js
@@ -0,0 +1,5 @@
+const latestVersion = {
+    version: "1.114.0",
+    description: "Новая версия OknaRed с улучшенной производительностью и новыми функциями:\n- Улучшенная файловая система\n- Новые приложения\n- Исправлены ошибки",
+    url: "https://fleen5l77.github.io/RedStore/latest.fsimage.orim.js"
+};
